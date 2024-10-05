7244f4e3074bf8f6472ff6bf2e63c01899aec3a8
Andrew <andrewjacobkennedy@gmail.com>
2024-10-05 11:21:28 -0700

    wip

--- entities/camera_controller.py
+++ entities/camera_controller.py
@@ -14,9 +14,17 @@ class CameraController(Entity):
         self.camera_speed = 1
         self.epsilon_x = 20
         self.epsilon_y = 10
+        self.x_min = 0
+        self.x_max = 0
+        self.y_min = 0
+        self.y_max = 0
 
     def start(self) -> None:
         self.player = self.find("Player")
+        self.x_min = -400
+        self.x_max = 400 - self.scene.main_camera.resolution[0]
+        self.y_min = -200
+        self.y_max = 200 - self.scene.main_camera.resolution[1]
 
     def update(self) -> None:
         delta = self.player.position().to_vector2() - self.scene.main_camera.center()
@@ -28,5 +36,5 @@ class CameraController(Entity):
         if abs(dy) > self.epsilon_y:
             self.scene.main_camera.y += ny
 
-        self.scene.main_camera.x = pmath.clamp(self.scene.main_camera.x, -400, 400)
-        self.scene.main_camera.y = pmath.clamp(self.scene.main_camera.y, -200, 200)
+        self.scene.main_camera.x = pmath.clamp(self.scene.main_camera.x, self.x_min, self.x_max)
+        self.scene.main_camera.y = pmath.clamp(self.scene.main_camera.y, self.y_min, self.y_max)
--- entities/player.py
+++ entities/player.py
@@ -1,3 +1,5 @@
+from math import atan2, degrees
+
 from potion import *
 
 
@@ -7,32 +9,42 @@ class Player(Entity):
         self.name = "Player"
 
         # Sprite
-        self.sprite = AnimatedSprite.from_atlas("atlas.png", "player")
-        self.sprite.pivot.set_center()
+        self.sprite = Sprite.empty()
+        self.sprites = [
+            Sprite.from_atlas("atlas.png", "player.0001"),
+            Sprite.from_atlas("atlas.png", "player.0002"),
+            Sprite.from_atlas("atlas.png", "player.0003"),
+            Sprite.from_atlas("atlas.png", "player.0004"),
+            Sprite.from_atlas("atlas.png", "player.0005"),
+            Sprite.from_atlas("atlas.png", "player.0006"),
+            Sprite.from_atlas("atlas.png", "player.0007"),
+            Sprite.from_atlas("atlas.png", "player.0008"),
+        ]
+        for s in self.sprites:
+            s.pivot.set_center()
 
         # Movement
-        self.move_input_timer = 0
         self.move_input = False
+        self.move_input_timer = 0
         self.move_direction = Vector2.zero()
         self.move_speed = 0
         self.max_move_speed = 1
         self.brake_multiplier = 2
-
-        self.move_frame = 0
-        self.move_target: Point | None = None
         self.mx = 0
         self.my = 0
+        self.facing_angle = 0
 
     def update(self) -> None:
         self.update_move_input()
         self.update_move_direction()
         self.update_movement()
+        self.update_facing_angle()
+        self.update_sprite()
         self.zsort()
 
     def update_move_input(self) -> None:
         if Mouse.get_left_mouse():
             self.move_input = True
-            self.move_target = Mouse.world_position()
         else:
             self.move_input = False
 
@@ -61,57 +73,34 @@ class Player(Entity):
         if self.my:
             self.move_y(self.my)
 
-        return
+    def update_facing_angle(self) -> None:
+        self.facing_angle = degrees(atan2(self.move_direction.y, self.move_direction.x))
 
-        # Clamp move timer
-        self.move_input_timer = pmath.clamp(self.move_input_timer, 0, 10)
+    def update_sprite(self) -> None:
+        self.sprite.frame = int(Engine.frame() % 4)
 
-        # Set move speed
-        if self.move_input_timer == 0:
-            self.move_speed = 0
-        elif self.move_input_timer < 1:
-            self.move_speed = 1
-        elif self.move_input_timer < 3:
-            self.move_speed = 2
-        elif self.move_input_timer < 6:
-            self.move_speed = 3
-        elif self.move_input_timer < 10:
-            self.move_speed = 4
+        if -13 < self.facing_angle < 13:
+            i = 0
+        elif 13 < self.facing_angle < 60:
+            i = 1
+        elif 60 < self.facing_angle < 120:
+            i = 2
+        elif 120 < self.facing_angle < 167:
+            i = 3
+        elif 167 < self.facing_angle <= 180:
+            i = 4
+        elif self.facing_angle <= -167:
+            i = 4
+        elif self.facing_angle <= -120:
+            i = 5
+        elif self.facing_angle <= -60:
+            i = 6
+        elif self.facing_angle <= -13:
+            i = 7
         else:
-            self.move_speed = 5
+            i = 0
 
-        # Get move direction
-        self.mx = 1
-        self.my = 0
-
-        # Accumulate move frames
-        self.move_frame += 1
-
-        # Figure out movement frames based on move speed:
-        if self.move_speed == 0:
-            frame_threshold = 0
-            self.move_frame = 0
-            self.mx = 0
-            self.my = 0
-        elif self.move_speed == 1:
-            frame_threshold = 8
-        elif self.move_speed == 2:
-            frame_threshold = 6
-        elif self.move_speed == 3:
-            frame_threshold = 4
-        elif self.move_speed == 4:
-            frame_threshold = 2
-        else:
-            frame_threshold = 1
-
-        # Move if we've accumulated enough frames
-        if self.move_frame >= frame_threshold:
-            self.move_frame = 0
-            if self.mx:
-                self.move_x(self.mx)
-            if self.my:
-                self.move_y(self.my)
-
+        self.sprite = self.sprites[i]
 
     def zsort(self) -> None:
         self.z = self.y * -1
